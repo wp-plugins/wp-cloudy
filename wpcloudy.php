46d49deeb1803359eb8faa849c5c735b31686ddb
rainbowgeek <rainbowgeek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-20 15:36:16 +0000

    stable readme
    git-svn-id: https://plugins.svn.wordpress.org/wp-cloudy/trunk@759362 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpcloudy.php
+++ wpcloudy.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Cloudy
 Plugin URI: http://wpcloudy.com/
 Description: WP Cloudy is a powerful weather plugin for WordPress, based on Open Weather Map API, using Custom Post Types and shortcodes, bundled with a ton of features.
-Version: 1.0
+Version: 1.1
 Author: Benjamin DENIS
 Author URI: http://wpcloudy.com/
 License: GPLv2
