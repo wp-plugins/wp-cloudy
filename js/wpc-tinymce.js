636f7ad6f4c781047a0ae81f6d6a75ceadabe579
rainbowgeek <rainbowgeek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-09 20:35:15 +0000

    version 3.4.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-cloudy/trunk@1241911 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/wpc-tinymce.js
+++ js/wpc-tinymce.js
@@ -1,30 +1,11 @@
 jQuery(document).ready(function($) {
-	//TinyMCE v3.x
-    tinymce.create('tinymce.plugins.wpc_plugin', {
-        init : function(ed, url) {
-                // Register command for when button is clicked
-                ed.addCommand('wpc_insert_shortcode', function() {
-                    var number = prompt("Enter the id of the weather to display: "), 
-                    shortcode;
-					if (number !== null) {
-						number = parseInt(number);
-						shortcode = '[wpc-weather id="' + number + '"/]';
-						ed.execCommand('mceInsertContent', 0, shortcode);
-					}
-                });
-
-            // Register buttons - trigger above command when clicked
-            ed.addButton('wpc_button', {title : 'Insert Weather', cmd : 'wpc_insert_shortcode' });
-        },   
-    });
-
     //TinyMCE v4.x
 	(function() {
 	    tinymce.PluginManager.add('wpc_button_v4', function( editor, url ) {
 	        editor.addButton( 'wpc_button_v4', {
 	            title: 'WP Cloudy',
 	            type: 'button',
-	            icon: 'icon mceIcon mce_wpc_button',
+	            icon: 'icon mceIcon dashicons-before dashicons-cloud',
 	            onclick: function() {
 				    editor.windowManager.open( {
 				        title: 'Insert Weather',
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://wpcloudy.com/
 Tags: weather, forecast, openweathermap, weather widget, hour forecast, local weather, sunset, sunrise, wind, weather map
 Requires at least: 3.8
 Tested up to: 4.3
-Stable tag: 3.4
+Stable tag: 3.4.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -105,6 +105,14 @@ All the answers are on our site (at least we try): http://wpcloudy.com
 6. WP Cloudy in action
 
 == Changelog ==
+= 3.4.1 =
+* FIX 5 notices: Undefined index (wpc-detectGeolocation, wpc-manualGeolocation, wpc-posLat, wpc-posLon, wpc-posCityId, wpc-posCityName), thanks to thib3113
+* FIX Improve performance in WP Admin
+* FIX Wrong label for 16 days forecast in global settings
+* INFO Set a default value for cache time (30 minutes)
+* INFO Improve UX by adding support links
+* INFO New logo for WP Cloudy using Dashicons font
+* INFO Remove TinyMCE v3 compatibility
 = 3.4 =
 * NEW Import/Export global settings, useful if you have multiple websites (weathers can be exported too via the Tools Exporter Screen)
 * INFO WordPress 4.3 compatibility
